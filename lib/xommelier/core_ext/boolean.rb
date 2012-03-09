10bea647ceaf76afcd395369909d59417d28f9a5
Artyom Semyonov <sevenov@gmail.com>
2012-03-10 02:50:35 +0400

    Added nil and empty string cast for Boolean

--- lib/xommelier/core_ext/boolean.rb
+++ lib/xommelier/core_ext/boolean.rb
@@ -3,7 +3,7 @@ require 'xommelier'
 class Boolean
   BOOLEAN_MAP = {
     true => true, 'true' => true, 'TRUE' => true, '1' => true, '1.0' => true, 1 => true, 1.0 => true,
-    false => false, 'false' => false, 'FALSE' => false, '0' => false, '0.0' => false, 0 => false, 0.0 => false
+    false => false, 'false' => false, 'FALSE' => false, '0' => false, '0.0' => false, 0 => false, 0.0 => false, nil => false, '' => false
   }
 
   def self.from_xommelier(value)
