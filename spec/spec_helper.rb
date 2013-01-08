9b8bfaee7b8ca4b28dce8d20bffdc70d3b09be4b
Alexander Semyonov <al@semyonov.us>
2013-01-08 08:35:10 +0400

    Run specs in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,8 @@ ATOM_XMLNS = 'http://www.w3.org/2005/Atom'
 
 RSpec.configure do |config|
   config.mock_with :rspec
+  config.order = 'random'
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
