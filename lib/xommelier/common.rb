c0d5c8f757a471d87a1a6331c4abc6b95f18c1d6
Alexander Semyonov <al@semyonov.us>
2013-04-08 08:53:36 +0400

    Do not raise exceptions on nil values in Time822

--- lib/xommelier/common.rb
+++ lib/xommelier/common.rb
@@ -5,6 +5,7 @@ module Xommelier
     # A date-time displayed in RFC 822 format.
     class Time822 < Time
       def self.from_xommelier(value)
+        return unless value
         case value
         when String
           value = rfc2822(value) rescue parse(value)
