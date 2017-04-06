833cc506d0230997c6e619fab41b4057b752f24c
Alex Semyonov <alex@semyonov.us>
2017-04-07 00:00:06 +0300

    Remove deprecated RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,6 @@ ATOM_XMLNS = 'http://www.w3.org/2005/Atom'.freeze
 RSpec.configure do |config|
   config.mock_with :rspec
   config.order = 'random'
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
